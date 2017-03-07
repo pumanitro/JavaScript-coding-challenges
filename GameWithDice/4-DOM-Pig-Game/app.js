cd01e0375eb13393fa70ce05c94ca06828deaaaf
pumanitro <pumanitro@gmail.com>
2017-03-07 20:09:53 +0100

    Repaired gameInit function. Now show player names and hide winner sign and class.

--- GameWithDice/4-DOM-Pig-Game/app.js
+++ GameWithDice/4-DOM-Pig-Game/app.js
@@ -75,6 +75,14 @@ function gameInit()
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
 
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+
+    document.querySelector('.player-0-panel').classList.add('active');
+
 
 }
 
