84bd244f2e9f2a128c6580ac40884d05e2a1912b
pumanitro <pumanitro@gmail.com>
2017-03-07 11:25:13 +0100

    Reset values for 0 and hide dice at start.

--- GameWithDice/4-DOM-Pig-Game/app.js
+++ GameWithDice/4-DOM-Pig-Game/app.js
@@ -8,3 +8,12 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
