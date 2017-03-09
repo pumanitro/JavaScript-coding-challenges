d2c348e6a124b50cdc06113c0a44bd58403d72e9
pumanitro <pumanitro@gmail.com>
2017-03-09 16:56:16 +0100

    Added object responsive for question collection and getting a random one.

--- Advanced/script.js
+++ Advanced/script.js
@@ -10,4 +10,18 @@ var questionName = new Question("What is my name ?",["Patryk","Oskar","Piotr"],0
 var questionDogName = new Question("What is my dog name ?",["Pinia","Max","Kolo"],0);
 var questionColor = new Question("What is my favourite color ?",["Green","Blue","Red"],1);
 
-var allQuest = [questionName,questionDogName,questionColor];
+var allQuestions = {
+    listOfQuestions : [questionName,questionDogName,questionColor],
+    questionNumber : 0,
+    showRandomQuestion : function(){
+      var randPos = Math.floor(Math.random() * 3);
+      this.questionNumber++;
+
+      console.log(this.questionNumber+'. '+this.listOfQuestions[randPos].question);
+      console.log('a) '+this.listOfQuestions[randPos].answers[0]);
+      console.log('b) '+this.listOfQuestions[randPos].answers[1]);
+      console.log('c) '+this.listOfQuestions[randPos].answers[2]);
+    }
+};
+
+allQuestions.showRandomQuestion();
