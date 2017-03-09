b910929159a07d1ddd95bb28810673200bda4b55
pumanitro <pumanitro@gmail.com>
2017-03-09 21:40:48 +0100

    Transformed code into module by IIFE.

--- Advanced/script.js
+++ Advanced/script.js
@@ -1,49 +1,53 @@
-//question - string, answers - array of strings, correctAnswer - int (array index)
-var Question = function(question,answers,correctAnswer)
-{
-    this.question = question;
-    this.answers = answers;
-    this.correctAnswer = correctAnswer;
+(function quizInConsole(){
 
-    this.isCorrect = function(answer){
+    //question - string, answers - array of strings, correctAnswer - int (array index)
+    var Question = function(question,answers,correctAnswer)
+    {
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
 
-        var ansIndex;
+        this.isCorrect = function(answer){
 
-        if(answer == 'a') ansIndex = 0;
-        else if(answer == 'b') ansIndex = 1;
-        else ansIndex = 2;
+            var ansIndex;
 
-        return ansIndex == this.correctAnswer;
+            if(answer == 'a') ansIndex = 0;
+            else if(answer == 'b') ansIndex = 1;
+            else ansIndex = 2;
+
+            return ansIndex == this.correctAnswer;
+        };
     };
-};
 
-var questionName = new Question("What is my name ?",["Patryk","Oskar","Piotr"],0);
-var questionDogName = new Question("What is my dog name ?",["Pinia","Max","Kolo"],0);
-var questionColor = new Question("What is my favourite color ?",["Green","Blue","Red"],1);
+    var questionName = new Question("What is my name ?",["Patryk","Oskar","Piotr"],0);
+    var questionDogName = new Question("What is my dog name ?",["Pinia","Max","Kolo"],0);
+    var questionColor = new Question("What is my favourite color ?",["Green","Blue","Red"],1);
 
-var allQuestions = {
-    listOfQuestions : [questionName,questionDogName,questionColor],
-    questionNumber : 0,
-    showRandomQuestion : function(){
-      var randPos = Math.floor(Math.random() * 3);
-      this.questionNumber++;
+    var allQuestions = {
+        listOfQuestions : [questionName,questionDogName,questionColor],
+        questionNumber : 0,
+        showRandomQuestion : function(){
+          var randPos = Math.floor(Math.random() * 3);
+          this.questionNumber++;
 
-      console.log(this.questionNumber+'. '+this.listOfQuestions[randPos].question);
-      console.log('a) '+this.listOfQuestions[randPos].answers[0]);
-      console.log('b) '+this.listOfQuestions[randPos].answers[1]);
-      console.log('c) '+this.listOfQuestions[randPos].answers[2]);
+          console.log(this.questionNumber+'. '+this.listOfQuestions[randPos].question);
+          console.log('a) '+this.listOfQuestions[randPos].answers[0]);
+          console.log('b) '+this.listOfQuestions[randPos].answers[1]);
+          console.log('c) '+this.listOfQuestions[randPos].answers[2]);
 
-      return this.listOfQuestions[randPos];
+          return this.listOfQuestions[randPos];
+        }
+    };
+
+    var choosenQuestion = allQuestions.showRandomQuestion();
+
+    var answer = prompt("Please enter correct answer (a,b,c)");
+
+    if (answer) {
+        if(choosenQuestion.isCorrect(answer))
+            console.log('Correct answer!');
+        else
+            console.log('Uncorrect answer :(');
     }
-};
 
-var choosenQuestion = allQuestions.showRandomQuestion();
-
-var answer = prompt("Please enter correct answer (a,b,c)");
-
-if (answer) {
-    if(choosenQuestion.isCorrect(answer))
-        console.log('Correct answer!');
-    else
-        console.log('Uncorrect answer :(');
-}
+})();
