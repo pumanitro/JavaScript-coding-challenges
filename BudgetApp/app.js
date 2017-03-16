4d7bd460269c20fcb8de8484d95ca1bd91e4e56a
pumanitro <pumanitro@gmail.com>
2017-03-16 13:13:36 +0100

    Added initialization function which initialize whole project.

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -31,7 +31,20 @@ var UIController = (function(){
 
 var controller = (function(budgetCtrl,UICtrl){
 
-    var DOM = UICtrl.getDOMStrings();
+    var setupEventListeners = function(){
+
+        var DOM = UICtrl.getDOMStrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {
+
+            //For older browsers event.which == 13
+            if(event.keyCode == 13 || event.which == 13)
+                ctrlAddItem();
+
+        });
+    };
 
     var ctrlAddItem = function()
     {
@@ -50,14 +63,13 @@ var controller = (function(budgetCtrl,UICtrl){
 
     };
 
-    document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-
-        //For older browsers event.which == 13
-        if(event.keyCode == 13 || event.which == 13)
-            ctrlAddItem();
-
-    });
+    return{
+        init: function () {
+            console.log('Application has started.');
+            setupEventListeners();
+        }
+    }
 
 })(budgetController,UIController);
+
+controller.init();
