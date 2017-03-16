3e0e80aacc6e98c6f3a328da9905a5b1b1f44c76
pumanitro <pumanitro@gmail.com>
2017-03-16 10:05:08 +0100

    Added key pressing handler. And created function for adding an item to our list.

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -8,18 +8,30 @@ var UIController = (function(){
 
 var controller = (function(budgetCtrl,UICtrl){
 
-document.querySelector('.add__btn').addEventListener('click',function() {
+    var ctrlAddItem = function()
+    {
+        //1. Get the input data
 
-    //1. Get the input data
+        //2. Add the item to the budget controller
 
-    //2. Add the item to the budget controller
+        //3. Add the item to the UI
 
-    //3. Add the item to the UI
+        //4. Calculate the buget
 
-    //4. Calculate the buget
+        //5. Display the budget in the UI
 
-    //5. Display the budget in the UI
+        console.log('It works');
 
-});
+    };
+
+    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+
+        //For older browsers event.which == 13
+        if(event.keyCode == 13 || event.which == 13)
+            ctrlAddItem();
+
+    });
 
 })(budgetController,UIController);
