cf7e79f60c1f5dde929fd03b9e570ab44b8aa787
pumanitro <pumanitro@gmail.com>
2017-03-16 12:59:54 +0100

    Putted hardcoded class names into object and return it with get input object which have all input data.

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -4,13 +4,41 @@ var budgetController = (function(){
 
 var UIController = (function(){
 
+    var DOMStrings = {
+
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+
+    };
+
+    return{
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            }
+        },
+
+        getDOMStrings : function(){
+            return DOMStrings;
+        }
+    }
+
 })();
 
 var controller = (function(budgetCtrl,UICtrl){
 
+    var DOM = UICtrl.getDOMStrings();
+
     var ctrlAddItem = function()
     {
         //1. Get the input data
+
+        var input = UICtrl.getInput();
+        console.log(input);
 
         //2. Add the item to the budget controller
 
@@ -20,11 +48,9 @@ var controller = (function(budgetCtrl,UICtrl){
 
         //5. Display the budget in the UI
 
-        console.log('It works');
-
     };
 
-    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
 
