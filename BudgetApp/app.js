316183f31491f706e11c0588ffb568f25f3cd3d4
pumanitro <pumanitro@gmail.com>
2017-03-18 15:51:47 +0100

    Now are updated UI of percenteges of all expenses.

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -145,7 +145,8 @@ var UIController = (function(){
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel:  '.item__percentage'
 
     };
 
@@ -217,6 +218,26 @@ var UIController = (function(){
             el.parentNode.removeChild(el);
         },
 
+        displayPercanteges: function(percentages) {
+
+            var fields = document.querySelectorAll(DOMStrings.expensesPercLabel);
+            
+            var nodeListForEach  = function (list, callback) {
+                for(var i=0; i < list.length; i++)
+                {
+                    callback(list[i],i);
+                }
+            };
+
+            nodeListForEach(fields, function (current, index) {
+                if(percentages[index] > 0)
+                    current.textContent = percentages[index] + '%';
+                else
+                    current.textContent = '---';
+            });
+            
+        },
+
         getDOMStrings : function(){
             return DOMStrings;
         }
@@ -263,7 +284,7 @@ var controller = (function(budgetCtrl,UICtrl){
         var percentages = budgetCtrl.getPercentages();
 
         //3. Update the UI with the new percentages
-        console.log(percentages);
+        UICtrl.displayPercanteges(percentages);
 
     };
 
