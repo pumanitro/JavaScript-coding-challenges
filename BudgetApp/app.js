d507cbc7c4f18013f048fa1f59fe2435ebe71408
pumanitro <pumanitro@gmail.com>
2017-03-19 14:49:02 +0100

    Added data into the webpage.

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -146,7 +146,8 @@ var UIController = (function(){
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel:  '.item__percentage'
+        expensesPercLabel:  '.item__percentage',
+        dateLabel: '.budget__title--month'
 
     };
 
@@ -267,6 +268,20 @@ var UIController = (function(){
             
         },
 
+        displayMonth: function () {
+
+            var now,year,month, months;
+
+            now = new Date();
+
+            months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+            month= now.getMonth();
+            year = now.getFullYear();
+
+            document.querySelector(DOMStrings.dateLabel).textContent = months[month] + ' ' + year;
+
+        },
+
         getDOMStrings : function(){
             return DOMStrings;
         }
@@ -382,6 +397,7 @@ var controller = (function(budgetCtrl,UICtrl){
                 percentage: -1
 
             });
+            UICtrl.displayMonth();
             setupEventListeners();
         }
     }
